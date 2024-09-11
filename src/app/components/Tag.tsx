52cf57c85e16439ba500360c8bbe26fa9771af7e
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-09-11 23:57:22 +0200

    font updated

--- src/app/components/Tag.tsx
+++ src/app/components/Tag.tsx
@@ -4,7 +4,7 @@ interface TagProps {
 
   const Tag = ({label} : TagProps) => {
     return (
-        <span className="bg-orange-100 text-orange-500 border-orange-500 border text-xs  px-2 py-1 rounded-full capitalize">{label}</span>
+        <span className="tag bg-orange-100 text-orange-500 border-orange-500 border text-xs  px-2 py-1 rounded-full capitalize">{label}</span>
     );
 }
 
