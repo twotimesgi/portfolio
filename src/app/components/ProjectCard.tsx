04e8bc82f737dae00b9279774e5536b653166bcb
luigibardellagerbi <luigibardellagerbi@mail.com>
2024-09-26 00:02:11 +0200

    Add obj fit cover on proj images

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard = ({ image, title, description, tags, link }: ProjectCardProps
     <div className="hover:bg-white/20 hover:shadow-lg rounded-lg w-full transition-all group ">
       <a href={link} className="px-6 sm:py-6 mb-6 flex flex-col sm:gap-5 sm:flex-row gap-2">
       <div className="text-slate-400 sm:w-[150px] w-full pt-1 text-nowrap shrink-0 ">
-        <Image src={image} className="rounded-md border-[3px] border-slate-300 group-hover:border-slate-500 transition-all group-hover:scale-105" width={300} alt={`${title} Screenshot`} />
+        <Image src={image} className="rounded-md border-[3px] border-slate-300 group-hover:border-slate-500 transition-all group-hover:scale-105 w-[300px] h-[90px] object-cover" width={300} alt={`${title} Screenshot`} />
       </div>
       <div>
           <span className="h4 group-hover:text-orange-500 text-slate-700 text-lg font-semibold transition">
